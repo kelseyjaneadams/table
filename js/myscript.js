a98f3c6f410afa99a7e4337e99ca66beac5a6058
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-09-27 13:56:59 +0000

    Add jquery to manipulate the DOM

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("h2").css("text-decoration", "underline"); //underlines all <h2> elements 
+    $("ul").css("border", "solid 1px #ccc"); // adds a border to all lists
+});
+
+// How can we do this without using css styles inline?
