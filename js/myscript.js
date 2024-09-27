8cbb55e567f0cb7d0b2a469e51e1616efae43924
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2024-09-27 14:04:12 +0000

    Save js file

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,11 @@
-$(document).ready(function () {
-    $("h2").css("text-decoration", "underline"); //underlines all <h2> elements 
-    $("ul").css("border", "solid 1px #ccc"); // adds a border to all lists
-});
+// $(document).ready(function () {
+//     $("h2").css("text-decoration", "underline"); //underlines all <h2> elements 
+//     $("ul").css("border", "solid 1px #ccc"); // adds a border to all lists
+// });
 
 // How can we do this without using css styles inline?
+
+$(document).ready(function () {
+    $("h2").addClass("underline"); // Adds underline class to all <h2> elements
+    $("ul").addClass("border"); // Adds border class to all lists
+});
